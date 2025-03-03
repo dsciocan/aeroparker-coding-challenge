8994c8a03ff985718f9a7e8a54a881c33653183a
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-03-03 12:46:09 +0000

    first commit

---
+++ src/test/java/com/dsc/aeroparker_coding_challenge/AeroparkerCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.aeroparker_coding_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroparkerCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
