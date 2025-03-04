813062aba7d541e8493feca885a3813fdb07b1fe
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-03-04 11:51:37 +0000

    feat: add findByEmailAddress repository method

--- src/main/java/com/dsc/aeroparker_coding_challenge/repository/UserRepository.java
+++ src/main/java/com/dsc/aeroparker_coding_challenge/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.dsc.aeroparker_coding_challenge.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findByEmailAddress(String emailAddress);
 }
